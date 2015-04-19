b4333ed0255bdb37dbb097034a7a3b27e244c7e8
Jacob Salazar <jacobs100cool@gmail.com>
2015-04-19 20:28:42 +0200

    Working version, solution of the PDE in 1D

--- Examples/code_figures/Function_generator.m
+++ Examples/code_figures/Function_generator.m
@@ -2,16 +2,12 @@
 % Working script to plot the function 
 % x3 ?6x2 +11x?6
 
-x=linspace(1,3,50);
+x=linspace(0.5,3.5,50);
 
-
-
-handF1 = plot(x,x.^3-6*x.^2+11*x-6);
-[c,h,v] = StdFigForm(handF1,'Solution equation', 'x', 'y');
+handF1 = figure()
+plot(x,x.^3-6*x.^2+11*x-6);
+[c,h,v] = StdFigForm('Solution equation', 'x', 'y');
 set(h,'FontWeight', 'Normal');
 set(v,'FontWeight', 'Normal');
 
-
-
-
-
+hold on;
